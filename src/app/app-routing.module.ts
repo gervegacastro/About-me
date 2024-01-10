44031e621c3898327316de8e9bd8d41f05eb8ce2
gervegacastro <ger.vegacastro@gmail.com>
2024-01-10 10:59:34 -0300

    V 1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sobre-mi', component: SobreMiComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'sobre-mi'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobreMiComponent,
+    NavbarComponent,
+    ProyectosComponent,
+    TecnologiasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
